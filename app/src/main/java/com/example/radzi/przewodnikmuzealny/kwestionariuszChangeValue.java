90472bfd25eed2a9df15675de17340feb5aace9a
radziq1302 <radziq1302@gmail.com>
2017-06-18 12:31:25 +0200

    20170618
    wpisywanie sumy pkt do formularza na ftp

--- app/src/main/java/com/example/radzi/przewodnikmuzealny/kwestionariuszChangeValue.java
+++ app/src/main/java/com/example/radzi/przewodnikmuzealny/kwestionariuszChangeValue.java
@@ -18,7 +18,7 @@ public class kwestionariuszChangeValue extends StringRequest {
         params = new HashMap<>();
         params.put("username", username);
         params.put("password", password);
-        params.put("kwestionariusz", kwestionariusz+"");
+        params.put("kwestionariusz", kwestionariusz + "");
 
 
     }
--- kwestionariuszChangeValue1.php
+++ kwestionariuszChangeValue1.php
@@ -2,9 +2,9 @@
     	$con = mysqli_connect("localhost", "id1606400_id1553152_admin", "qwerty1tomek", "id1606400_ludz");
 	$username = $_POST["username"];
     $password = $_POST["password"];
-	$kwestionariusz;
-	$statement = mysqli_prepare($con, "UPDATE user SET kwestionariusz=1 WHERE username = ? AND password = ?");
-	mysqli_stmt_bind_param($statement, "ss", $username, $password);
+	$kwestionariusz=$_POST["kwestionariusz"];
+	$statement = mysqli_prepare($con, "UPDATE user SET kwestionariusz = ? WHERE username = ? AND password = ?");
+	mysqli_stmt_bind_param($statement, "iss", $kwestionariusz, $username, $password);
 	mysqli_stmt_execute($statement);
 	
 
